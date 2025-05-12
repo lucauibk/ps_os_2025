e3deae5f119cd14b08f9fe6256cdedf3dd03cd45
lucauibk <150453615+lucauibk@users.noreply.github.com>
2025-05-12 12:33:20 +0200

    ex7

--- exercise07/task_3/thread_pool.c
+++ exercise07/task_3/thread_pool.c
@@ -1,17 +1,92 @@
 #include "thread_pool.h"
 
-void pool_create(thread_pool* pool, size_t size){
-#error "TODO"
+#include "thread_pool.h"
+
+static struct job_queue_entry* create_job(job_function function, job_arg argument) {
+    static job_id next_id = 1;
+    struct job_queue_entry* job = malloc(sizeof(struct job_queue_entry));
+    if (!job) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    job->id = next_id++;
+    job->function = function;
+    job->argument = argument;
+    job->next = NULL;
+    job->status = JOB_PENDING;
+    pthread_mutex_init(&job->mutex, NULL);
+    pthread_cond_init(&job->cond, NULL);
+    return job;
+}
+
+static void* thread_worker(void* arg){
+    thread_pool* pool = (thread_pool*)arg;
+    while(1){
+        pthread_mutex_lock(&pool->queue_mutex);
+        while(pool->queue == NULL && !pool->shutdown){
+            pthread_cond_wait(&pool->queue_not_empty, &pool->queue_mutex);
+        }
+        if(pool->shutdown){
+            pthread_mutex_unlock(&pool->queue_mutex);
+            break;
+        }
+        struct job_queue_entry* job = pool->queue;
+        pool->queue = job->next;
+        pthread_mutex_unlock(&pool->queue_mutex);
+
+        job->function(job->argument);
+        
+        pthread_mutex_lock(&job->mutex);
+        job->status = JOB_FINISHED;
+        pthread_cond_signal(&job->cond);
+        pthread_mutex_unlock(&job->mutex);
+    }
+}
+
+void pool_create(thread_pool* pool, size_t size) {
+    pool->threads = malloc(size * sizeof(pthread_t));
+    if (!pool->threads) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    pool->size = size;
+    pool->queue = NULL;
+    pool->shutdown = false;
+
+    pthread_mutex_init(&pool->queue_mutex, NULL);
+    pthread_cond_init(&pool->queue_not_empty, NULL);
+
+    for (size_t i = 0; i < size; i++) {
+        pthread_create(&pool->threads[i], NULL, thread_worker, pool);
+    }
 }
 
 job_id pool_submit(thread_pool* pool, job_function start_routine, job_arg arg) {
-#error "TODO"
+    struct job_queue_entry* new_job = create_job(start_routine, arg);
+    if(pool->queue == NULL){
+        pool->queue = new_job;
+    }else{
+        struct job_queue_entry* current = pool->queue;
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = new_job;
+    }
+    return new_job->id; // Return the unique ID of the job
 }
 
 void pool_await(job_id id) {
-#error "TODO"
+    
 }
 
 void pool_destroy(thread_pool* pool) {
-#error "TODO"
+    struct job_queue_entry* current = pool->queue;
+    while (current != NULL) {
+        struct job_queue_entry* next = current->next;
+        free(current);
+        current = next;
+    }
+    free(pool->threads);
+    pool->threads = NULL;
+    pool->queue = NULL;
 }
--- exercise07/task_3/thread_pool.h
+++ exercise07/task_3/thread_pool.h
@@ -2,17 +2,34 @@
 #define THREAD_POOL_H
 
 #include <stddef.h> // for size_t
+#include <pthread.h> // for pthread_t and related functions
+#include <stdbool.h> // for bool type
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 typedef void* (*job_function)(void*);
 typedef void* job_arg;
-typedef /* TODO */ job_id;
+typedef size_t job_id;
 
 /***
  * This is the stub of a simple job queue.
  */
+typedef enum{
+	JOB_PENDING,
+	JOB_RUNNING,
+	JOB_FINISHED,
+} job_status;
 
 struct job_queue_entry {
-	// TODO: Design the contents for a queue, which stores jobs.
+	job_id id;
+	job_function function;
+	job_arg argument;
+	struct job_queue_entry* next;
+	// Synchronisation für `pool_await`
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    job_status status;
 };
 
 /***
@@ -21,7 +38,12 @@ struct job_queue_entry {
  */
 
 typedef struct {
-	// TODO: Design the contents of a thread pool
+	pthread_t * threads;
+	size_t size; // Number of threads in the pool
+	struct job_queue_entry * queue;
+	pthread_mutex_t queue_mutex;
+    pthread_cond_t queue_not_empty;
+    bool shutdown;
 } thread_pool;
 
 // Prototypes for REQUIRED functions
