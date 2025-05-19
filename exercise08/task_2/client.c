f443e0f8075494b67e94d61a2946da72a1c97f21
lucauibk <150453615+lucauibk@users.noreply.github.com>
2025-05-19 11:41:14 +0200

    commit1

--- exercise08/task_2/client.c
+++ exercise08/task_2/client.c
@@ -21,12 +21,12 @@ int main(int argc, char *argv[]) {
 
     int port = atoi(argv[1]);
     const char *username = argv[2];
-    int sockfd;
+    int clientfd;
 
     struct sockaddr_in server_addr;
 
     // Erstellen des Sockets
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -36,16 +36,16 @@ int main(int argc, char *argv[]) {
     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);  // localhost
 
     // Verbindung zum Server herstellen
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(clientfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("connect");
-        close(sockfd);
+        close(clientfd);
         exit(EXIT_FAILURE);
     }
 
     // Benutzername an den Server senden
-    if (send(sockfd, username, strlen(username), 0) < 0) {
+    if (send(clientfd, username, strlen(username), 0) < 0) {
         perror("send");
-        close(sockfd);
+        close(clientfd);
         exit(EXIT_FAILURE);
     }
 
@@ -63,13 +63,13 @@ int main(int argc, char *argv[]) {
 
         // Wenn /quit eingegeben wird, Verbindung beenden
         if (strcmp(buffer, "/quit") == 0) {
-            send(sockfd, "%s disconnected", username, 0);
+            send(clientfd, "%s disconnected", username, 0);
             break;
         }
 
         // Wenn /shutdown eingegeben wird, Nachricht an den Server senden
         if (strcmp(buffer, "/shutdown") == 0) {
-            if (send(sockfd, "/shutdown", 9, 0) < 0) {
+            if (send(clientfd, "/shutdown", 9, 0) < 0) {
                 perror("send /shutdown");
                 break;
             }
@@ -78,12 +78,12 @@ int main(int argc, char *argv[]) {
         }
 
         // Normale Nachricht senden
-        if (send(sockfd, buffer, strlen(buffer), 0) < 0) {
+        if (send(clientfd, buffer, strlen(buffer), 0) < 0) {
             perror("send");
             break;
         }
     }
 
-    close(sockfd);
+    close(clientfd);
     return 0;
 }
