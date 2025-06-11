621f363a9933a92ee5a75cd85565d18cda97abd2
lucauibk <150453615+lucauibk@users.noreply.github.com>
2025-06-11 09:23:43 +0200

    e

--- exercise11/task_2/xor_string.c
+++ exercise11/task_2/xor_string.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char* transform(const char* input) {
     size_t len = strlen(input);
     char* result = strdup(input);
 
     for (size_t i = 0; i < len; i++) {
-        result[i] = input[i] ^ 0x20;
+        if (isalpha((unsigned char)input[i])) {
+            result[i] = input[i] ^ 0x20;
+        }
     }
 
     return result;
